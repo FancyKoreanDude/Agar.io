497bb35136f04889a004b2441a7ad5b8f646e106
FancyKoreanDude <ethan.chong@brentwood.ca>
2015-11-04 12:46:03 -0800

    Nov 4 2015

---
+++ src/agar/io/Map.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agar.io;
+
+import environment.Environment;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author EthanChong
+ */
+class Map extends Environment {
+
+    public Map() {
+        
+    }
+
+    @Override
+    public void initializeEnvironment() {
+        
+    }
+
+    @Override
+    public void timerTaskHandler() {
+        
+    }
+
+    @Override
+    public void keyPressedHandler(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleasedHandler(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void environmentMouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void paintEnvironment(Graphics graphics) {
+        
+    }
+    
+}
